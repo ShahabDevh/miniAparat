db26bf1a4d70c22dc5a72e57f1e464a921ac4f55
shahabhosseini <shahab.hosseini2699@gmail.com>
2020-02-28 01:32:29 +0330

    styled the global style

--- src/asset/GlobalStyle.js
+++ src/asset/GlobalStyle.js
@@ -1,5 +1,33 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: "IRANSans";
+  src: url("./font/IRANSans_0.ttf?family=IRANSans");
+}
+ 
+body {  
+  direction: rtl; 
+  font-family: "IRANSans";
+}
 
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+
+  &::-webkit-scrollbar {
+    width: 0.5em;
+  }
+
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 4px rgba(0, 0, 0, 0.2);
+  }
+  
+  &::-webkit-scrollbar-thumb {
+    border-radius: 18px;
+    background-color: #5d5d5d;
+    outline: 1px solid #708090;
+  }
+}
 `;
