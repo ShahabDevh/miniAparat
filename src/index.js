11c8fe453904bd69172ad057903574f5a5401196
shahabhosseini <shahab.hosseini2699@gmail.com>
2020-02-29 21:56:41 +0330

    remove style from index file and moved the fonts to public folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import MainPage from "./MainPage";
-import "./style/index.css";
 
 ReactDOM.render(
   <Router>
